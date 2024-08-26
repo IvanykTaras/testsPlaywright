5bb5c7e5c6bdba353e1f9dcce6812479fd40086c
TarasIvanyk2002 <tarasivanyk001@gmail.com>
2024-08-26 16:32:44 +0200

    end interaction

--- tests/basicinteraction.test.ts
+++ tests/basicinteraction.test.ts
@@ -1,4 +1,4 @@
-import test, { expect } from "@playwright/test";
+import {test, expect } from "@playwright/test";
 
 test("Interaction with inputs", async ({page})=>{
     await page.goto("https://www.lambdatest.com/selenium-playground/simple-form-demo");
@@ -8,4 +8,22 @@ test("Interaction with inputs", async ({page})=>{
     console.log("before entering " + await messageInput.inputValue());
     await messageInput.type("Hi man");
     console.log("after entering " + await messageInput.inputValue());
-})
+});
+
+test("Sum", async ({page})=>{
+    page.goto("https://www.lambdatest.com/selenium-playground/simple-form-demo");
+    const sum1input = await page.locator("#sum1");
+    const sum2input = await page.locator("#sum2");
+
+    const getValueBtn = await page.locator('//button[contains(text(),"Get Sum")]');
+
+
+    let num1 = 121;
+    let num2 = 546;
+    await sum1input.type(num1.toString());
+    await sum2input.type(num2.toString());
+    await getValueBtn.click();
+    const result = await page.locator("#addmessage");
+    console.log(await result.textContent());
+    await expect(result).toHaveText((num1 + num2).toString());
+});
